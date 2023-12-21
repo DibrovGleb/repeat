d2ed28d6c768fc79b82862588b94c8aa51e95787
Dibrov Gleb <g.t.ru@mail.ru>
2023-12-21 11:48:40 +0300

    Small update

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const {log} = console
-const ppl = "Hello im simple shrimp string"
+function digitalClock(){
+    let dateFunction = new Date(),
+    day = dateFunction.getDay(),
+    hours = dateFunction.getHours(),
+    minutes = dateFunction.getMinutes(),
+    seconds = dateFunction.getSeconds(),
+    timeFormat = 'AM',
+    todayDay = document.querySelector(`.weekdays :nth-child(${day})`)
+    
+    
+    timeFormat = hours >= 12 ? 'PM' : 'AM'
+    hours = hours  < 10 ? '0' + hours : hours
+    minutes = minutes < 10 ? '0' + minutes : minutes
+    seconds = seconds < 10 ? '0' + seconds : seconds
+    document.querySelector('.hours').innerHTML = hours
+    document.querySelector('.minutes').innerHTML = minutes
+    document.querySelector('.seconds').innerHTML = seconds
+    document.querySelector('.format').innerHTML = timeFormat
+    todayDay.classList.add('active')
+}
 
-const b = document.getElementById("monka").innerHTML = ppl;
+setInterval(digitalClock,1000)
